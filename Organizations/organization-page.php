84dc0d81dc703d7d498a969949e13c8e30081746
redlikedeath <charles.bonn@mines.sdsmt.edu>
2016-04-23 14:31:17 -0600

    sorted list and information passing

--- Organizations/organization-page.php
+++ Organizations/organization-page.php
@@ -2,7 +2,7 @@
 <html lang="en-US">
 
 <head> 
-    <title> <?php echo $organization_name;  ?> </title>
+    <title> <?php echo $_GET['org'];  ?> </title>
     <meta charset = "utf-8" />
 </head>
 
@@ -10,14 +10,36 @@
 <body>
     
     
- <?php
-    $variable = $_GET['var'];
+    <?php
+        //load correct page data
+        $orgpage = $_GET['org'];
+
+        if (file_exists('organization-list.xml')) {
+            $xml = simplexml_load_file('organization-list.xml');
+            //print_r($xml);
+        } else {
+            exit('Failed to open test.xml.');
+        }
+    
+        foreach($xml->children() as $organization) {
+            if ( strcmp( $organization->name , $orgpage) == 0){
+                break;
+            }
+        }
     ?>
     
- Welcome <?php echo $_GET['var']; ?><br>
-<?php
-echo "hello";
-?>
+    name <?php echo $organization->name; ?><br>
+    icon <?php echo $organization->icon; ?><br>
+    advisor <?php echo $organization->advisor; ?><br>
+    president <?php echo $organization->president; ?><br>
+    officers <?php echo $organization->officers; ?><br>
+    members <?php echo $organization->members; ?><br>
+    description <?php echo $organization->description; ?><br>
+    
+    Welcome <?php echo $_GET['org']; ?><br>
+    <?php
+        echo "hello $organization->name";
+    ?>
 
     
 </body>
--- Organizations/organization-template.php
+++ Organizations/organization-template.php
@@ -22,46 +22,29 @@
 <?php
 
 
-//link to new page
-$link = 'organization-page.php';
+    //link to new page
+    $link = 'organization-page.php';
+    $page = array();
     
-if (file_exists('organization-list.xml')) {
-    $xml = simplexml_load_file('organization-list.xml');
- 
-    //print_r($xml);
-} else {
-    exit('Failed to open test.xml.');
-}
+    if (file_exists('organization-list.xml')) {
+        $xml = simplexml_load_file('organization-list.xml');
+     //print_r($xml);
+    } else {
+        exit('Failed to open test.xml.');
+    }
     
-//$xml=simplexml_load_file('organizaton-list.xml') or die("Error: Cannot create object");
-foreach($xml->children() as $organization ) {
-        //echo "<a href='$link' name='$organization->name'>$organization->name</a> <br>";
-       // $_POST[]
-        //echo "<a href=\"javascript:formSubmit($organization->name);\">$organization->name</a>\n <br>";
-    //echo "<a href=\"javascript:formSubmit('$organization->name);\">$organization->name</a>";
+    //put xml into array
+    foreach($xml->children() as $org) {
+        $page[] =  $org;
+    }
+    
+    //sort list
+    ksort($page, SORT_STRING );
+    
+    foreach($page as $organization ) {
+
+        echo "<a href='$link?org=$organization->name' >$organization->name</a> <br>";
         
-         //echo $organizaton->name . ", ";
-    
-        //echo "<form action='$link' method=POST>";
-        //echo "<input type='hidden' name='name' value='$organization->name' class='inline'>";
-        
-        //echo "<a type='submit' href='$link' name='$organization->name'>$organization->name</a>";
-        //echo "<input type='text' size='10' name='name' value='$organization->name'>";
-        //echo "<input type=submit name=submit value=''>";
-        //echo "</form>";
-    
-        //echo "<FORM METHOD='get' NAME='myForm' id='$$organization->name'>";
-        //echo "<INPUT TYPE='HIDDEN' NAME='bob' VALUE='value1'>";
-        //echo "<INPUT TYPE='HIDDEN' NAME='NAME2' VALUE='value2'>";
-        //echo "</FORM>";
-    
-                                        //document.forms[\"myForm\"].submit();return false;
-        echo "<a href='$link?var=$organization->name'>$organization->name</a> <br>";
-        
-    
-       
-    
-          
     }
 ?>
     
